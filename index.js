9feda3a1f85f6b70f211f1682a0737941cbcd433
David-ImpulseWD <djgm1206@gmail.com>
2018-10-25 20:28:55 -0400

    Add Documentation

--- index.js
+++ index.js
@@ -1,64 +1,71 @@
 #!/usr/bin/env node
-process.title = "HTML Translator";
 
+process.title = "HTML Translator"; //Process Title
+
+//Const
 const VueCompiler = require("./Lib").VueCompiler,
 	  ReactCompiler = require("./Lib").ReactCompiler,
-
-	  FF = require("./utils").TranslatorFileFunctions,
+	  FF = require("./file-utils").TranslatorFileFunctions,
 	  {startServer} = require("./server"),
-
 	  {join} = require("path");
 
-//React or Vue
-const mode = process.argv[2];
+const mode = process.argv[2]; //Get the mode to compile
+
 if (mode === "-h" || mode === "--help" || !mode){
+	//Help Commands
     console.log(
     `Use: node index.js [command] [input-file] [output-folder]
     	  node index.js -s [port]
 
     Commands:
-        Compile to react:    --react | -r
-        Compile to vue:      --vue   | -v
-        Help:                --help  | -h`
-    )
-    process.exit(1);
+    	Start dev server:	 -s
+        Compile to react:    -r
+        Compile to vue:      -v
+        Help:                -h`
+    );
+    process.exit(1); //Stop execution
+
 } else if (
 	mode === "-v" || 
-	mode === "--vue" || 
-	mode === "-r" || 
-	mode === "--react"
-	) {
+	mode === "-r"
+) {
 
-	var entryPoint;
-	//dirname for html file
+	var fileName;
 	
 	if (process.argv.length <= 3){
 		console.log(`Please select a file to compile.`);
 		process.exit(1);
 	} else {
-		entryPoint = join(__dirname, process.argv[3]);
+		fileName = join(__dirname, process.argv[3]); //Get the file path
 	}
-	//dirname for output folder
+
+	//Dirname for the output folder
 	const output = process.argv[4] ? join(__dirname, process.argv[4]) : join(__dirname, "dist");
 	
+	//Instance the File Functions Class
 	const functions = new FF();
-	functions.setParams(entryPoint, output);
+	functions.setParams(fileName, output); //Set Input file and Output folder
 
-	let input = process.argv[3].match(/\w*(?=.html$)/);
-	var name = input[0][0].toUpperCase() + input[0].slice(1);
+	let componentName = process.argv[3].match(/((\w*-)*\w*|\w*)(?=.html$)/); //Get the name from the file
+
+	var name = componentName[0].split("-").map(e=>{
+        return e[0].toUpperCase() + e.slice(1);
+    }).join(""); //convert to CamelCase
 	
-	var file = functions.getFile();
-	var js = functions.getJs();
-	var css = "";
+	var file = functions.getFile(); //Get the html file data
+	var js = functions.getJs(); //Get the JS file data
+	var css = functions.getCSS(); //Get the CSS file data
 	
-	var compiled = {};
-	if (mode === "--vue" || mode === "-v"){
+	var compiled;
+	if (mode === "-v"){
 		compiled = {
-			content: VueCompiler(name, file, css, js),
+			name,/*File Name*/
+			content: VueCompiler(name, file, css, js),/*File Content*/
 			type: "vue"
 		}
-	} else if (mode === "--react" || mode === "-r"){
+	} else if (mode === "-r"){
 		compiled = {
+			name,
 			content: ReactCompiler(name, file, css, js),
 			type: "react"
 		}
@@ -66,11 +73,17 @@ if (mode === "-h" || mode === "--help" || !mode){
 		console.error("Invalid Mode.");
 		process.exit(1);
 	}
-	functions.writeFile(compiled);
+	functions.writeFile(compiled); //Append Files to the output folder
 	console.log(`Thanks for use html compiler.\n\nOpen "${output}" to view your files.`);
 } else if(mode === "--server" || "-s"){
-	let port = process.argv[3] || 8080;
-	startServer(port);
+
+	let port = parseInt(process.argv[3]); //Get if the port is a number
+
+	if (port.toString() === "NaN") {
+		console.log("Port should be a Number");
+	} else {
+		startServer(port);
+	}
 } else {
 	console.error(`Invalid Mode "${mode}"\n\nValid modes are -v and -r.\nPlease attemp with thats.`);
 }
