7b03d0d55c583cdf9e2cce07ad0dc91f23e079f8
David-ImpulseWD <djgm1206@gmail.com>
2019-02-18 14:36:33 -0400

    Created Commons File Functions

---
+++ Lib/commons/file.js
@@ -0,0 +1,19 @@
+const {readFileSync} = require("fs");
+
+class File {
+
+	/**
+	 * Read File As String
+	 * 
+	 * @static
+	 * @param {String} path 
+	 * 
+	 * @return {String}
+	 */
+	static readFileAsString(path) {
+		const buff = readFileSync(path);
+		return buff.toString();
+	}
+}
+
+module.exports = File;
